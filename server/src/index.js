b49ab5307a21b3d66699987620ee671064795605
Federico Matarrita Arata <federico@usematics.com>
2018-10-25 19:42:50 -0600

    add crosss domain access

--- server/src/index.js
+++ server/src/index.js
@@ -54,7 +54,11 @@ connection.on('open', function () {
 
 
 
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use('/api/', lepidopteraRouter)
 
 app.listen(port, () => {
