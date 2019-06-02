d01d3fd82531038063f9ab3aced7a519b7eff384
Federico Matarrita Arata <federico@usematics.com>
2019-06-02 04:20:07 -0600

    Add family route

--- server/src/Routers/lepidoptera.js
+++ server/src/Routers/lepidoptera.js
@@ -1,82 +1,153 @@
 import express from 'express'
 //import Lepidoptera from '../models/lepidopteraModel'
 import Lepidoptera from "../models/lepidoptera";
+
 const lepidopteraRouter = express.Router();
 const mapProjection = {
-    'Collection Date':1,
-    'Herbivore family':1,
-    'Herbivore species':1,
-    'herbivore genus name':1,
-    'Herbivore subfamily':1,
-    'Latitude':1,
-    'Locality':1,
-    'Longitude':1,
-    'primary ecosystem':1,
-    'Sector':1,
-    'voucher':1
+    "Herbivore species": 1,
+    "voucher": 1,
+    "Locality": 1,
+    "Collection Date": 1,
+    "Latitude": 1,
+    "Longitude": 1,
+    "Herbivore family": 1
 };
+
+const familyProjection ={
+    "Herbivore family": 1,
+    "Herbivore subfamily":1,
+}
 const groupCount = {
-    _id:"$Herbivore species",
-    number:{$sum:1}
+    _id: "$Herbivore species",
+    number: {$sum: 1}
 }
 
 const options = {
-    $sort:{'Herbivore species':1}
+    $sort: {'Herbivore species': 1}
 };
-
-
-
 lepidopteraRouter
-    .get('/:voucher', (req,res) =>{
-        Lepidoptera.find({'voucher':req.params.voucher},mapProjection,options,(err,lepidoptera) =>{
+    .get('/voucher/:voucher', (req, res) => {
+        console.log("vocuher")
+
+        Lepidoptera.find({'voucher': req.params.voucher}, mapProjection, options, (err, lepidoptera) => {
             res.json(lepidoptera)
         })
     })
-    .get('/species/:species', (req,res) =>{
+    .get('/species/:species', (req, res) => {
+        console.log("species")
+
         let species = req.params.species
             .split(",")
-            .map(function (sp){
-                return {'Herbivore species':sp.trim()}
-        });
+            .map(function (sp) {
+                return {'Herbivore species': sp.trim()}
+            });
         console.log(species);
         Lepidoptera.aggregate([
-            {
-                $match:{
-                    $or :species
-                }
-            },
-            {
-                $group: {
-                    _id:"$Herbivore species",
-                    count:{$sum:1},
-                    "family":{"$first":"$Herbivore family"},
-                    records:{
-                        "$push":{
-                            voucher: "$voucher",
-                            "collectionDate": "$Collection Date",
-                            longitude: "$Longitude",
-                            latitude: "$Latitude",
-                         //   sector:"$Sector"
+                {
+                    $match: {
+                        $or: species
+                    }
+                },
+                {
+                    "$project": mapProjection
+                },
+
+                {
+                    $group: {
+                        _id: {
+                            "herb": "$Herbivore species",
+                            locality: "$Locality",
+                        },
+                        count: {$sum: 1},
+                        "family": {"$first": "$Herbivore family"},
+                        "data": {"$push": "$$ROOT"}
+                    }
+                },
+                {
+                    $group: {
+                        _id: "$_id.herb",
+                        count: {$sum: "$count"},
+                        data: {
+                            "$push": {
+                                "k": "$_id.locality",
+                                "v": "$data",
+
+                            }
                         }
                     }
+                },
+                {
+                    "$addFields": {
+                        "data": {"$arrayToObject": "$data"}
+                    }
+                },
+                {
+                    $sort: {
+                        _id: 1
+                    }
                 }
-            },
-                {$sort:{_id:-1}}
             ]
-            ,function (err,lepidoptera) {
-                if (err){
+            , function (err, lepidoptera) {
+                if (err) {
                     throw err;
-                }else{
+                } else {
+
                     res.json(lepidoptera)
                 }
             }
         );
-    })
-    .get('/', (req,res) => {
-        Lepidoptera.find({},(err,lepidoptera) =>{
+    })//species
+
+    .get('/', (req, res) => {
+        console.log("root");
+        Lepidoptera.find({}, (err, lepidoptera) => {
             res.json(lepidoptera)
         })
             .limit(1000)
-    });
+    })
+
+    .get('/family/', (req, res) => {
+        Lepidoptera.aggregate([
+                {
+                    "$project": familyProjection
+                },
+                {
+                    $group: {
+                        _id: {
+                            "family": "$Herbivore family",
+                            "subfamily": "$Herbivore subfamily"
+                        },
+                        count:{$sum:1}
+                    }
+                },
+                {
+                  $group:{
+                      _id:"$_id.family",
+                      subfamilies:{
+                          $push:{
+                              sufamily:"$_id.subfamily",
+                              count:{$sum:"$count"}
+                          }
+                      },
+                      count:{$sum:"$count"}
+                  },
+                },
+                {
+                    $sort:{_id:1}
+                }
+            ],
+            function (err, lepidoptera) {
+                if (err) {
+                    throw err;
+                } else {
+
+                    res.json(lepidoptera)
+                }
+            }
+        )//aggregate
+    })
+
+;
+
 
 export default lepidopteraRouter
