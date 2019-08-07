93a3dbcab12954c78f46316172d3ecb43f734aec
Federico Matarrita Arata <federico@usematics.com>
2019-08-07 11:04:31 -0600

    Change import to use only csvtojson.

--- server/mongo/import.sh
+++ server/mongo/import.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
-
-
 #Remove periods and symbols form header, delimit fields by " and separator ,
-
 #remove header from filemaker export
-tail -n +2 gcavoucher-lista.csv  > headless.csv
-echo "Headles\n"
-cat headers.csv headless.csv >> importdata.csv
-echo "megred files\n"
-npm i -g csvtojson
-csvtojson importdata.csv  > importdata.json
-rm -rf headless.csv
+#tail -n +2 gcavoucher-lista.csv  > headless.csv
+#echo "Headles\n"
+#cat headers.csv headless.csv >> importdata.csv
+#echo "megred files\n"
+#npm i -g csvtojson
+#csvtojson importdata.csv  > importdata.json
+#rm -rf headless.csv
 #mongoimport -vv -d testimport -c lepidoptera --type json --file importdata.csv --headerline
-mongoimport -vv -d inventory -c lepidoptera --type json --file importdata.json --jsonArray
+#mongoimport -vv -d inventory -c lepidopteras --type json --file importdata.json --jsonArray
+
+
+#june12.csv
+headers = '["lepBin","adultLiveWtG","adultVoucherFate","amnhCode","anotherPara","barcodeLength","barcodeWithN","blank1","class","collectedGroup","collectedGroupTrait","collectionDate","collector","continent","country","dateDnaSample","dateStamp","detHerb","detHost","detHyperpara","detPara","dhjPlantColl","dhjParVoucher","dnaSampleTo","dump","east","elevation","entry","forewingLengthMm","foundAs","fpdes","gelatinCapsuleToWhom","herbEcloDate","herbPrepDate","herbPupDate","herbivoreComment","herbivoreFamily","herbivoreGenBankAcc","herbivoreGenusName","herbivoreSex","herbivoreSpecies","herbivoreSubfamily","herbivoreTribe","hostFamily","hostSpecies","hyperBin","hyperDhjParVoucher","hyperparasiteFamily","hyperparasiteSpecies","hyperparasiteSubfamily","idenBasisHerb","idenBasisHost","idenBasisPara","immatureParts","kingdom","latitude","locality","longitude","mostRecentDateNjTreeIdentified","multiple","north","numberWithinYear","order","otherVoucherNumber","paraBin","parasiteFamily","parasiteGenusName","parasiteOrder","parasiteSpecies","parasiteSubfamily","parasiteTribe","photo","phylum","plantNickname","primaryEcosystem","province","pupalLiveWtG","rearingOutcome","scannedPhoto","search","secondaryEcosystem","sector","specimenDepositedAt","stage","standardUnit","subtribe","timeStamp","type","voucher","whenEnter","whoEnterData","year"]'
+csvtojson --headers='["lepBin","adultLiveWtG","adultVoucherFate","amnhCode","anotherPara","barcodeLength","barcodeWithN","blank1","class","collectedGroup","collectedGroupTrait","collectionDate","collector","continent","country","dateDnaSample","dateStamp","detHerb","detHost","detHyperpara","detPara","dhjPlantColl","dhjParVoucher","dnaSampleTo","dump","east","elevation","entry","forewingLengthMm","foundAs","fpdes","gelatinCapsuleToWhom","herbEcloDate","herbPrepDate","herbPupDate","herbivoreComment","herbivoreFamily","herbivoreGenBankAcc","herbivoreGenusName","herbivoreSex","herbivoreSpecies","herbivoreSubfamily","herbivoreTribe","hostFamily","hostSpecies","hyperBin","hyperDhjParVoucher","hyperparasiteFamily","hyperparasiteSpecies","hyperparasiteSubfamily","idenBasisHerb","idenBasisHost","idenBasisPara","immatureParts","kingdom","latitude","locality","longitude","mostRecentDateNjTreeIdentified","multiple","north","numberWithinYear","order","otherVoucherNumber","paraBin","parasiteFamily","parasiteGenusName","parasiteOrder","parasiteSpecies","parasiteSubfamily","parasiteTribe","photo","phylum","plantNickname","primaryEcosystem","province","pupalLiveWtG","rearingOutcome","scannedPhoto","search","secondaryEcosystem","sector","specimenDepositedAt","stage","standardUnit","subtribe","timeStamp","type","voucher","whenEnter","whoEnterData","year"]' jun12.csv  > jun12.json
+mongoimport -vv -d inventory -c lepidopteras --type json --file jun12.json --jsonArray
