045091cd4711721705e6b9574b89e6828a73573e
Federico Matarrita Arata <federico@usematics.com>
2019-08-13 23:02:39 -0600

    comments and dev connection

--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@ const bodyParser = require('body-parser');
 var config = require('config');
 const lepidopteraRouter = require ('./Routers/lepidoptera');
 
-
+//Express App setup
 const app = express();
 const port = config.get('app.port');
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-const mongoUri = `mongodb://${config.get('db.user')}:${config.get('db.password')}@${ config.get('db.host')}:${config.get('db.port')}/${config.get('db.database')}`
-
+//Connect to Mongo, allow development black password
+const user = (config.get('db.user')!=""&&config.get('db.password')!="")?`${config.get('db.user')}:${config.get('db.password')}@`:"";
+const mongoUri = `mongodb://${user}${ config.get('db.host')}:${config.get('db.port')}/${config.get('db.database')}`;
 var connection = mongoose.connect(mongoUri, { useNewUrlParser: true }, (error) => {
     if (error){
         console.log('Error connecting to database');
@@ -22,44 +22,17 @@ var connection = mongoose.connect(mongoUri, { useNewUrlParser: true }, (error) =
     }
 }).connection;
 
-
-/*connection.on('open', function () {
-    connection.db.db.listCollections().toArray(function (err, names) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(names);
-        }
-
-        mongoose.connection.close();
-    });
-});*/
-//connection.on('error', console.error.bind(console, 'connection error:'));
-
-/// create a connection to the DB
-/*var connection = mongoose.createConnection(
-    mongoUri, {useNewUrlParser: true});*/
-/*
-connection.on('open', function () {
-    // connection established
-    new Admin(connection.db).listDatabases(function (err, result) {
-        console.log('listDatabases succeeded');
-        // database list stored in result.databases
-        var allDatabases = result.databases;
-        console.log(allDatabases)
-    });
-});*/
-
-
-
+//Allow cross origins
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
+//Add Routers
 app.use('/api/', lepidopteraRouter);
 
+//Listen on port
 app.listen(port, () => {
     console.log(`listening on port ${ port }`);
 });
-
