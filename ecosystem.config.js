d5140863a7bee816625bbd1dca38a782b98a2bf1
Federico Matarrita Arata <federico@usematics.com>
2020-01-07 17:31:39 -0500

    add pm2 ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    apps : [{
+        name: 'lepidoptera',
+        script: 'src/index.js',
+        
+        // Options reference: d    instances: 1,
+        autorestart: true,
+        watch: false,
+        instance_var: 'INSTANCE_ID',
+        max_memory_restart: '1G',
+        error_file: '/srv/log/node/error-api.acguanacaste.ac.cr.log',
+        out_file: '/srv/log/node/out-api.acguanacaste.ac.cr.log',
+        log_file: '/srv/log/node/log-api.acguanacaste.ac.cr.log',
+        env: {
+            PORT: 3000,
+            NODE_ENV: 'production',
+            NODE_CONFIG_DIR: "/srv/www/api.acguanacaste.ac.cr/config/"
+            
+        },
+        env_production: {
+            NODE_ENV: 'production',
+            PORT: 3000,
+            NODE_CONFIG_DIR: "/srv/www/api.acguanacaste.ac.cr/config/"
+        }
+    }]
+    
+};
